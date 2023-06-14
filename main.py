00717d7c906561a1702018546c51553a0cb63069
omer358 <omermaki358@gmail.com>
2023-06-14 20:30:37 +0200

    lowest term gcd

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
 # Press the green button in the gutter to run the script.
-from fragment import Fragment
+
+from fraction import Fraction
+
 
 if __name__ == '__main__':
-    myFragment = Fragment(2, 3)
-    print(myFragment)
+    fragment1 = Fraction(1, 4)
+    fragment2 = Fraction(1, 2)
+    print(fragment1.__add__(fragment2))
