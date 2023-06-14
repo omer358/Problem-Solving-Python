d7a517cbbc114a2c860911848db2826a01143192
omer358 <omermaki358@gmail.com>
2023-06-14 19:57:48 +0200

    rename fraction class

--- fraction.py
+++ fraction.py
@@ -6,3 +6,6 @@ class Fragment:
     def __str__(self):
         # print the fraction in the form a/b
         return "{}/{}".format(self.num, self.den)
+
+    def __add__(self, other_fraction: Fragment):
+        new_num = self.num*other_fraction.den
