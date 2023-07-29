543a20ab18253f8a55546008c6b570d981004266
omer358 <omermaki358@gmail.com>
2023-07-29 03:17:32 +0200

    pop operations on known size list

--- performance_data_structure/lists_performance.py
+++ performance_data_structure/lists_performance.py
@@ -1,3 +1,6 @@
+from timeit import Timer
+
+
 def test1():
     my_list = []
     for i in range(1000):
@@ -16,3 +19,14 @@ def test3():
 
 def test4():
     my_list = list(range(1000))
+
+
+if __name__ == '__main__':
+    pop_end = Timer("x.pop()", "from __main__ import x")
+    pop_zero = Timer("x.pop(0)", "from __main__ import x")
+
+    x = list(range(2000000))
+    print(pop_zero.timeit(number=1000))
+
+    x = list(range(2000000))
+    print(pop_end.timeit(number=1000))
