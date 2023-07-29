24f0b1bce934ec462eb9187562223c63cdf8db95
omer358 <omermaki358@gmail.com>
2023-07-29 03:27:09 +0200

    pop operations on different lists sizes

--- performance_data_structure/lists_performance.py
+++ performance_data_structure/lists_performance.py
@@ -22,11 +22,23 @@ def test4():
 
 
 if __name__ == '__main__':
+    # pop_end = Timer("x.pop()", "from __main__ import x")
+    # pop_zero = Timer("x.pop(0)", "from __main__ import x")
+    #
+    # x = list(range(2000000))
+    # print(pop_zero.timeit(number=1000))
+    #
+    # x = list(range(2000000))
+    # print(pop_end.timeit(number=1000))
+
+    # pop operations on a different size of lists
     pop_end = Timer("x.pop()", "from __main__ import x")
     pop_zero = Timer("x.pop(0)", "from __main__ import x")
+    print("\tpop(0)  pop()")
+    for i in range(1000000, 10000001, 1000000):
+        x = list(range(i))
+        pt = pop_end.timeit(number=1000)
 
-    x = list(range(2000000))
-    print(pop_zero.timeit(number=1000))
-
-    x = list(range(2000000))
-    print(pop_end.timeit(number=1000))
+        x = list(range(i))
+        pz = pop_zero.timeit(number=1000)
+        print("%15.5f, %15.5f" %(pz,pt))
