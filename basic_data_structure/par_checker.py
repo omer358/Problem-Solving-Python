bee31af09571d2029d47e6d0601bbf0673246b81
omer358 <omermaki358@gmail.com>
2023-08-31 12:30:37 +0200

    Balanced symbol problem

--- basic_data_structure/par_checker.py
+++ basic_data_structure/par_checker.py
@@ -22,5 +22,38 @@ def par_checker(symbol_string):
         return False
 
 
+def matches(open_s, close_s):
+    opens = "({["
+    closes = ")}]"
+    return opens.index(open_s) == closes.index(close_s)
+
+
+def par_checker_diff_symbols(symbol_string):
+    stack = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol in "({[":
+            stack.push(symbol)
+        else:
+            if stack.is_empty():
+                balanced = False
+            else:
+                top = stack.pop()
+                if not matches(top, symbol):
+                    balanced = False
+        index = index + 1
+    if balanced and stack.is_empty():
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
-    print(par_checker("((()(())))"))
+    # par_checker function for pretenses
+    # print(par_checker("((()(())))"))
+
+    # par_checker function for different symbols
+    print(par_checker_diff_symbols("[{{([][])}()}]"))
+    print(par_checker_diff_symbols("[{()]"))
