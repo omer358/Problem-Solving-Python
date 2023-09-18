e27ad8a7f710775425bd614e90a6a29b27b720c8
omer358 <omermaki358@gmail.com>
2023-09-18 17:26:31 +0200

    linked list impl, is_empty, add

---
+++ basic_data_structure/lists/unordered_list/unordered_list.py
@@ -0,0 +1,14 @@
+from node import Node
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
